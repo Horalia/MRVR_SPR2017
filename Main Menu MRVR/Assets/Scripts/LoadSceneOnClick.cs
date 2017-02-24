935b610cfaf78f18cb9a5605219f895735965cc1
Horalia <horaliaarmas@Horalias-MacBook-Pro.local>
2017-02-23 23:15:56 -0600

    Main Menu for MRVR

---
+++ Main Menu MRVR/Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,12 @@
+﻿/* From Unity Tutorial: Main Menu September 2016 presntation */
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	public void LoadByIndex(int sceneIndex)
+	{
+		SceneManager.LoadScene (sceneIndex);
+	}
+}
